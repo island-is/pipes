0d70c40a8944794d620212a8875d47e89d940e90
Jon Orn Arnarson <jonorn@andes.is>
2023-09-03 15:19:34 +0000

    fix: build for pipes core

--- apps/pipes/rollup.config.js
+++ apps/pipes/rollup.config.js
@@ -3,13 +3,16 @@ import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { readFile } from "node:fs/promises";
 
+const internalPackages = ["@island.is/zod", "@island.is/cleanup", "@island.is/pipes-module-core"];
 const currentPath = fileURLToPath(dirname(import.meta.url));
 /** @type {{main: string, dist: string, source: string, dependencies: Record<string, string>, peerDependencies: Record<string, string>, types: string}} */
 const packageJSON = JSON.parse(await readFile(join(currentPath, "package.json"), "utf-8"));
 const input = join(currentPath, ...packageJSON.source.replace("./", "").split("/"));
 const output = join(currentPath, ...packageJSON.main.replace("./", "").split("/"));
 const types = join(currentPath, ...packageJSON.types.replace("./", "").split("/"));
-const external = Object.keys({ ...packageJSON.dependencies, ...packageJSON.peerDependencies });
+const external = Object.keys({ ...packageJSON.dependencies, ...packageJSON.peerDependencies }).filter(
+  (e) => !internalPackages.includes(e),
+);
 export const files = {
   main: output,
   source: input,
