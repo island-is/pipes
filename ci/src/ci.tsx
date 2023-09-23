7604546781435b5ac2a2e2711917369a9db150c2
Jon Orn Arnarson <jonorn@andes.is>
2023-09-23 10:55:47 +0000

    fix: build

--- ci/src/ci.tsx
+++ ci/src/ci.tsx
@@ -3,12 +3,11 @@ import { createPipe } from "@island-is/pipes-core";
 import { GlobalConfig } from "./config.js";
 import { workspaceTestContext } from "./constraints/workspace-test.js";
 import { devImageInstallContext } from "./install/dev-image.js";
-import { releaseContext } from "./release/release.js";
 
 await createPipe(() => {
   const tasks = [devImageInstallContext, workspaceTestContext];
   if (GlobalConfig.action === "Release") {
-    return [...tasks, releaseContext];
+    return [...tasks];
   }
   if (GlobalConfig.action === "Test") {
     return [...tasks];
