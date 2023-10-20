c5e6145ebc77729049805214bd4f59748057e41f
lommi <jonorn@gmail.com>
2023-10-20 11:08:25 +0000

    fix: what

--- pipes-modules/pipes-module-node/src/context/publish.ts
+++ pipes-modules/pipes-module-node/src/context/publish.ts
@@ -62,7 +62,8 @@ export const NodePublish: removeContextCommand<PipesNodeModule["Context"]["Imple
   }
   const whoami = await fn(["whoami"]);
   console.log({ whoami });
-  await fn(["publish", "--access", props.access ?? "public"]);
+
+  //await fn(["publish", "--access", props.access ?? "public"]);
 
   return;
 };
